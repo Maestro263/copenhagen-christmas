c03f193f89a860fd9587db0e4f629cfa13920b85
Maestro263 <mohammedhassani1995@gmail.com>
2021-08-05 09:54:19 +0200

    First commit

---
+++ src/Components/Copenhagenparagraph.jsx
@@ -0,0 +1,29 @@
+const Copenhagenparagraph = () => {
+    return (
+
+
+        <p>
+            Alle juleeventyr har mindst et træk til fælles: De ender altid godt. 
+            Det bliver altid jul til sidst, uanset hvilke strabadser folk skal igennem. 
+            I 2010 overtog og genetablerede KCC juleudsmykningen i Indre By og oprettede anpartsselskabet Copenhagen Christmas.
+
+            Julen bringer glæde og håb til os alle – og varsler om lysere tider. Jul er gyldne stjerner. 
+            Julen er gran og guirlander, røde hjerte, fyldte kulørte shoppingposer og knitrende gavepapir. 
+            Man siger, at julen er hjerternes fest. Men julen er også de store forventningers fest. 
+            Julen er trængsel og alarm. I KCC gør vi, gennem Copenhagen Christmas, hvad vi kan, 
+            for at indfri de høje forventninger og skabe en eventyrlig atmosfære, 
+            fredelig og smittende julestemning. Vi ønsker at gøre Indre By til et samlingspunkt i juletiden. 
+            Vi vil tiltrække besøgende fra nær og fjern, og vi vil give folk lyst til at være – og blive – 
+            i København K. Vi ønsker at skabe en juledestination. Vi vil stemningstætte Indre By gennem både 
+            juleudsmykning og juleaktiviteter og respektere de symboler, som forbindes med julen. 
+            Samtidig ønsker vi at skabe fornyelse med respekt for traditionerne.
+
+            Vi glæder os hvert år til at byde julen velkommen til København K, 
+            og vi er altid på udkig efter nye gode julefortællinger – derfor ønsker vi også at udsmykke endnu 
+            flere gader og kvarterer. Etablering af juleudsmykningen, sker hovedsagligt på opfordring og i 
+            tæt samarbejde med butikkerne.
+        </p>
+    );
+}
+
+export default Copenhagenparagraph;
